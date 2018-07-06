c1fb2f1fe0bc865906da7c6d5d459df8239decd8
bart <bart.delrue@digipolis.gent>
2018-07-06 16:20:12 +0200

    VWW-7: Fetched and displayed dummy gentse-feesten data.

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,24 @@
       <h1 class="title">
         verwerkingsregister
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <ul class="grid-3">
+        <li v-for="item in items" :key="item" class="teaser">
+          <article>
+            <div class="content-bottom">
+              <h3>{{item.name.value}}</h3>
+              <p>
+                <a :href=item.url.value class="standalone-link">website <span
+                class="visually-hidden">van {{item.name.value}}</span></a> <br>
+                <strong>organisator </strong><span>{{item.contributor.value}}</span> <br>
+                <strong v-if="item.free.value == 1">gratis </strong>
+              </p>
+              <nuxt-link :to=" `verwerking/${item.id}` " class="standalone-link">
+                lees meer <span class="visually-hidden">over {{item.name.value}}</span>
+              </nuxt-link>
+            </div>
+          </article>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -17,10 +32,18 @@
     head () {
       return {
         title: 'verwerkingsregister'
-      }
+      };
     },
     meta: {},
-    components: {}
+    components: {},
+    async fetch ({store, params}) {
+      await store.dispatch('GET_ITEMS');
+    },
+    computed: {
+      items () {
+        return this.$store.state.items;
+      }
+    }
   };
 </script>
 
