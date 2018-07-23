a97ed25f017b1bad25c75b6b140f5fd4b24086aa
bart <bart.delrue@digipolis.gent>
2018-07-23 14:52:23 +0200

    VWW-50: Added footer and header organisms.

---
+++ components/organisms/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <div class="content-container">
+      <logo/>
+    </div>
+    <breadcrumbs/>
+  </header>
+</template>
+
+<script>
+  import logo from '~/components/molecules/logo';
+  import breadcrumbs from '~/components/molecules/breadcrumbs';
+
+  export default {
+    components: {logo, breadcrumbs},
+  };
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,19 @@
 <template>
   <div class="cs--default container">
-    <header class="header">
-      <div class="content-container">
-        <logo/>
-      </div>
-      <breadcrumbs/>
-    </header>
+    <v_header/>
     <main>
       <nuxt/>
     </main>
-    <footer class="footer" role="contentinfo">
-      <h2 class="visually-hidden">Footer</h2>
-      <div class="footer-top">
-        <div class="content-container">
-          <div class="footer--column">
-            <gent_info/>
-          </div>
-          <div class="footer--column">
-            <city_links/>
-          </div>
-          <div class="footer--column">
-            <post_address/>
-          </div>
-        </div>
-      </div>
-      <div class="footer-bottom content-container">
-        <div class="footer--column">
-          <partnership/>
-        </div>
-        <div class="footer--column">
-          <social_media/>
-        </div>
-      </div>
-    </footer>
+    <v_footer/>
   </div>
 </template>
 
 <script>
-
-  import logo from '~/components/molecules/logo';
-  import breadcrumbs from '~/components/molecules/breadcrumbs';
-  import gent_info from '~/components/molecules/gent-info';
-  import city_links from '~/components/molecules/city-links';
-  import post_address from '~/components/molecules/post-address';
-  import social_media from '~/components/molecules/social-media';
-  import partnership from '~/components/molecules/partnership';
+  import v_header from '~/components/organisms/header';
+  import v_footer from '~/components/organisms/footer';
 
   export default {
     middleware: ['breadcrumbs'],
-    components: {logo, breadcrumbs, gent_info, city_links, post_address, partnership, social_media},
-    computed: {}
+    components: {v_header, v_footer},
   };
 </script>
-
-<style>
-
-</style>
