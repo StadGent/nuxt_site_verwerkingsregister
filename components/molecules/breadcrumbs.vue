5a5006fab0fb761d8ed79f063003aed0ac4f42a7
bart <bart.delrue@digipolis.gent>
2018-07-26 11:15:37 +0200

    VWW-54: Updated all links to nuxt-links.

--- components/molecules/breadcrumbs.vue
+++ components/molecules/breadcrumbs.vue
@@ -4,8 +4,8 @@
       <h2 id="system-breadcrumb" class="visually-hidden">Breadcrumb</h2>
       <ol class="breadcrumb no-style">
         <li><a href="https://stad.gent">Stad.gent</a></li>
-        <li v-for="crumb in crumbs" :key="crumb">
-          <a :href="crumb.path">{{ crumb.label }}</a>
+        <li v-for="(crumb, index) in crumbs" :key="index">
+          <nuxt-link :to="crumb.path">{{ crumb.label }}</nuxt-link>
         </li>
         <li><span>Page title</span></li>
       </ol>
