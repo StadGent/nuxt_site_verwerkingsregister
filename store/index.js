2a57201cf3295fb433d019081521ff37c80dc887
bart <bart.delrue@digipolis.gent>
2018-08-21 11:49:51 +0200

    VWW-46: Updated sparql query to fetch relevant qa data.

--- store/index.js
+++ store/index.js
@@ -1,6 +1,40 @@
 import Vuex from "vuex"
 import axios from "axios"
 
+const SPARQL_QUERY =
+  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
+  "PREFIX schema: <http://schema.org/>" +
+  "PREFIX dcterms: <http://purl.org/dc/terms/>" +
+  "PREFIX gdv: <http://stad.gent/data/ns/data-processing/>" +
+  "SELECT" +
+  "  ?id" +
+  "  ?processor" +
+  "  ?type" +
+  "  ?name" +
+  "  (concat(group_concat(distinct ?personalData;separator=','),group_concat(distinct ?sensitivePersonalData;separator=',')) as ?personalData)" +
+  "  ?formal_framework" +
+  "  (concat(group_concat(distinct ?grantee;separator=',')) as ?grantees)" +
+  "FROM <http://stad.gent/data-processes/>" +
+  "FROM <http://stad.gent/agents/>" +
+  "WHERE {\n" +
+  "  ?verwerking a <http://data.vlaanderen.be/ns/toestemming#VerwerkingsActiviteit>;\n" +
+  "  dcterms:identifier ?id;\t\n" +
+  "  <http://data.vlaanderen.be/ns/toestemming#verwerkingsgrond> ?formal_framework .\n" +
+  "  ?verwerking <http://data.vlaanderen.be/ns/toestemming#verwerker>/skos:prefLabel ?processor;\n" +
+  "  dcterms:type/skos:prefLabel ?type;\n" +
+  "  dcterms:title ?name; \n" +
+  "  <http://stad.gent/data/ns/data-processing/grantee> ?grantee;\t\n" +
+  "  <http://stad.gent/data/ns/data-processing/hasPersonalData>/dcterms:type/skos:prefLabel ?personalData .\n" +
+  "  OPTIONAL { ?verwerking <http://stad.gent/data/ns/data-processing/hasSensitivePersonalData>/dcterms:type/skos:prefLabel ?sensitivePersonalData }\n" +
+  "}" +
+  "group by" +
+  "?verwerking" +
+  "?id" +
+  "?formal_framework" +
+  "?processor" +
+  "?type" +
+  "?name"
+
 export default () => {
   return new Vuex.Store({
     state: {
@@ -24,24 +58,24 @@ export default () => {
        * @constructor
        */
       async GET_ITEMS({ commit }) {
-        let items = await axios.get(
-          "https://stad.gent/sparql?default-graph-uri=&query=PREFIX+schema:+<http://schema.org/>SELECT+?event+?url+?name+?processor+?free+FROM+<http://stad.gent/gentse-feesten-2018/>+WHERE+{\t?event+a+schema:Event;+schema:name+?name;+schema:description+?description;+schema:url+?url;+schema:isAccessibleForFree+?free;+schema:contributor+?c.++\t?c+schema:name+?processor}"
-        )
-
         try {
-          items = items.data.results.bindings
-          // get the ID from the event value
-          items.map(item => {
-            const splitValues = item.event.value.split("/")
-            item.id = splitValues[splitValues.length - 1]
-            return item
+          let verwerkingen = await axios.get(
+            "https://qa.stad.gent/sparql?query=" +
+              encodeURIComponent(SPARQL_QUERY)
+          )
+
+          verwerkingen = verwerkingen.data.results.bindings
+          verwerkingen.map(verwerking => {
+            verwerking.grantees.value = verwerking.grantees.value.split(",")
+            verwerking.personalData.value = verwerking.personalData.value.split(
+              ","
+            )
           })
+
+          commit("SET_ITEMS", verwerkingen)
         } catch (error) {
           // todo show error
-          items = []
         }
-
-        commit("SET_ITEMS", items)
       }
     }
   })
