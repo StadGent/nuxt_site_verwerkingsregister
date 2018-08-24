a157a40d111265feafb18879ce62f111a0231e4c
bart <bart.delrue@digipolis.gent>
2018-08-24 11:12:59 +0200

    VWW-30: Added details page.

--- pages/verwerking/_id/index.vue
+++ pages/verwerking/_id/index.vue
@@ -1,22 +1,21 @@
 <template>
   <article class="content-container">
-    <h1>HALLO verwerking nummer {{ $route.params.id }}!</h1>
-
+    <h1>{{ details.name.value }}</h1>
     <div class="summary-box box-top">
       <div class="inner-box">
         <h2 class="visually-hidden">samenvatting</h2>
         <dl>
           <div>
             <dt>Categorie</dt>
-            <dd/>
+            <dd>{{ details.type.value }}</dd>
           </div>
           <div>
             <dt>Verwerkende dienst</dt>
-            <dd/>
+            <dd>{{ details.processor.value }}</dd>
           </div>
           <div>
             <dt>Rechtmatigheid</dt>
-            <dd/>
+            <dd>{{ details.formal_framework.value }}</dd>
           </div>
         </dl>
         <div class="accolade "/>
@@ -24,26 +23,29 @@
     </div>
 
     <h2>Beschrijving</h2>
-
+    <p>{{ details.description.value }}</p>
     <h2>Gegevens</h2>
     <h3>Over wie?</h3>
+    <p>{{ details.personalDataDescription.value }}</p>
     <h3>Welke persoonsgegevens?</h3>
     <ul>
-      <li/>
-      <li/>
-      <li/>
+      <li v-for="(value, index) in details.personalData.value" :key="index">{{ value }}</li>
     </ul>
     <h3>Welke gevoelige persoonsgegevens?</h3>
     <ul>
-      <li/>
-      <li/>
-      <li/>
+      <li v-for="(value, index) in details.sensitivePersonalData.value" :key="index">{{ value }}</li>
     </ul>
     <h2>Rechtmatigheid</h2>
     <h3>Type</h3>
+    <p>{{ details.formal_framework.value }}</p>
     <h3>Verduidelijking</h3>
+    <p>{{ details.formal_framework_clarification.value }}</p>
     <h2>Ontvangers</h2>
+    <ul>
+      <li v-for="(value, index) in details.grantees.value" :key="index">{{ value }}</li>
+    </ul>
     <h2>Bewaartermijn</h2>
+    <p>{{ details.storagePeriod.value }}</p>
 
   </article>
 </template>
@@ -57,6 +59,21 @@ export default {
   },
   meta: {
     breadcrumbs: [{ label: "verwerkingsregister", path: "/" }]
+  },
+  async fetch({ store, params }) {
+    // Only fetch items once
+    let id = params.id
+    if (id && !store.state.details[id]) {
+      await store.dispatch("GET_ITEM", id)
+    }
+  },
+  computed: {
+    details() {
+      let id = this.$route.params.id
+      if (id) {
+        return this.$store.state.details[id]
+      }
+    }
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -9,7 +9,6 @@ const LIMIT = 1000
 // Make axios enable caching
 const http = axios.create({
   baseURL: "/",
-  headers: { "Cache-Control": "no-cache" },
   // cache will be enabled by default
   adapter: cacheAdapterEnhancer(axios.defaults.adapter, {
     defaultCache: new LRUCache({ maxAge: SIX_HOURS })
@@ -54,11 +53,50 @@ const SPARQL_QUERY =
   "?type" +
   "?name"
 
+const DETAIL_QUERY = id => {
+  return `PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
+  PREFIX dcterms: <http://purl.org/dc/terms/> 
+  PREFIX gdv: <http://stad.gent/data/ns/data-processing/> 
+  SELECT  
+  ?id  
+  ?description 
+  ?processor  
+  ?type 
+  ?name 
+  ?personalDataDescription 
+  ?sensitivePersonalDataDescription 
+  (group_concat(distinct ?personalData;separator=',') as ?personalData) 
+  (group_concat(distinct ?sensitivePersonalData;separator=',') as ?sensitivePersonalData) 
+  ?formal_framework 
+  ?formal_framework_clarification 
+  (group_concat(distinct ?grantee;separator=',') as ?grantees) 
+  ?storagePeriod 
+  FROM <http://stad.gent/data-processes/> 
+  WHERE { 
+    ?verwerking a <http://data.vlaanderen.be/ns/toestemming#VerwerkingsActiviteit>; 
+    dcterms:identifier ?id; 
+    dcterms:description ?description; 
+    <http://data.vlaanderen.be/ns/toestemming#verwerkingsgrond>/skos:prefLabel ?formal_framework; 
+    gdv:formalFrameworkClarification ?formal_framework_clarification . 
+    ?verwerking <http://data.vlaanderen.be/ns/toestemming#verwerker>/skos:prefLabel ?processor; 
+    dcterms:type/skos:prefLabel ?type; 
+    dcterms:title ?name;  
+    <http://stad.gent/data/ns/data-processing/grantee>/skos:prefLabel ?grantee; 
+    dcterms:temporal/dcterms:title ?storagePeriod 
+    OPTIONAL { ?verwerking <http://stad.gent/data/ns/data-processing/hasPersonalData>/dcterms:type/skos:prefLabel ?personalData } 
+    OPTIONAL { ?verwerking <http://stad.gent/data/ns/data-processing/hasSensitivePersonalData>/dcterms:type/skos:prefLabel ?sensitivePersonalData } 
+    OPTIONAL { ?verwerking <http://stad.gent/data/ns/data-processing/hasPersonalData>/dcterms:description ?personalDataDescription } 
+    OPTIONAL { ?verwerking <http://stad.gent/data/ns/data-processing/hasSensitivePersonalData>/dcterms:description ?sensitivePersonalDataDescription } 
+    FILTER (?verwerking=<https://qa.stad.gent/id/data-process/${id}>) 
+  }`
+}
+
 export default () => {
   return new Vuex.Store({
     state: {
       breadcrumbs: [],
-      items: []
+      items: [],
+      details: {}
     },
     mutations: {
       SET_BREADCRUMBS(state, data) {
@@ -66,6 +104,9 @@ export default () => {
       },
       SET_ITEMS(state, data) {
         state.items = data
+      },
+      SET_ITEM(state, data) {
+        state.details[data.id.value] = data
       }
     },
     actions: {
@@ -107,7 +148,6 @@ export default () => {
                   )
                   .then(result => {
                     result = result.data.results.bindings
-                    console.log(result.cached, i)
 
                     // check for cached version
                     if (!result.cached) {
@@ -134,9 +174,41 @@ export default () => {
 
           let result = await Promise.all(promises)
           let verwerkingen = [].concat.apply([], result)
-          console.log(verwerkingen.length)
 
           commit("SET_ITEMS", verwerkingen)
+        } catch (error) {
+          console.error(error)
+          // todo show error
+        }
+      },
+      /**
+       * Fetch item details from the sparql endpoint.
+       *
+       * @param {Function} commit
+       * @returns {Promise.<void>}
+       * @constructor
+       */
+      async GET_ITEM({ commit }, id) {
+        let url = "https://qa.stad.gent/sparql"
+        if (process.env.DEPLOY_ENV === "production") {
+          url = "https://stad.gent/sparql"
+        }
+
+        try {
+          let result = await http.get(
+            url + "?query=" + encodeURIComponent(DETAIL_QUERY(id))
+          )
+          result = result.data.results.bindings[0]
+          if (!result.cached) {
+            result.grantees.value = result.grantees.value.split(",")
+            result.personalData.value = result.personalData.value.split(",")
+            result.sensitivePersonalData.value = result.sensitivePersonalData.value.split(
+              ","
+            )
+            result.cached = true
+          }
+
+          commit("SET_ITEM", result)
         } catch (error) {
           console.error(error)
           // todo show error
