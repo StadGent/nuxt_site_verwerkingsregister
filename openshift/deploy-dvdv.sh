46f27bfc4f42ef3bddc1f4284c5adb8124b7ab11
Matthias Ghijs <matthias.ghijs@digipolis.gent>
2019-08-29 16:11:06 +0200

    Added JenkinsfileDEV

---
+++ openshift/deploy-dvdv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+
+echo "Running as user: $(oc whoami)"
+echo ""
+echo "Changing project to verwerkingsregisterdv"
+oc project verwerkingsregisterdv
+
+echo "Importing nodejs S2I image"
+oc import-image registry.access.redhat.com/rhscl/nodejs-8-rhel7:latest --confirm || true
+
+echo "Processing template and creating"
+oc process -f openshift/verwerkingsregister-dvdv.yml | oc create -f - || true
+NOT_CREATED=$?
+
+if [ $NOT_CREATED ]; then
+  echo "Resources already exist, replacing"
+  oc process -f openshift/verwerkingsregister-dvdv.yml | oc replace -f - || true
+  echo "Starting builds"
+  oc start-build verwerkingsregister
+fi
+echo "Done."
