a9a992f1649862a95cf30be16ae2680a2bd3d5ff
Per Juchtmans <per.juchtmans@district09.gent>
2022-08-04 14:09:13 +0200

    HEAT-6495236 Change text 'introductietekst.vue'
    DPO requested textual changes to the "blue text area", motivation:
    
      Als DPO ben ik verantwoordelijk voor de privacyverklaring en ook het
      verwerkingsregister.
      De gevraagde tekstaanpassing moet meer duidelijkheid geven over het
      gedeeltelijk gepubliceerde interne CRM cerwerkingsregister.
      Aangezien dienst communicatie deze pagina niet kan aanpassen maar
      vermoedelijk wel team web is hiervoor een aanvraag ingediend.

--- components/introductietekst.vue
+++ components/introductietekst.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
     <p>
-      De Stad en het OCMW Gent vinden de bescherming van jouw persoonsgegevens erg belangrijk.<br>
-      Daarom vind je op deze pagina een overzicht van de verwerkingen van persoonsgegevens die we uitvoeren.
+      De Algemene Verordening Gegevensbescherming verplicht ons een intern register bij te houden met al onze verwerkingen van persoonsgegevens.
     </p>
     <p>
-      Wens je meer informatie over hoe we met jouw persoonsgegevens omgaan?<br>
+      De Stad en OCMW Gent vinden de bescherming van jouw persoonsgegevens erg belangrijk.
+    </p>
+    <p>
+      Daarom vind je op deze pagina een overzicht van de verwerkingen van de persoonsgegevens die we uitvoeren.
+    </p>
+    <p>
+      Dit overzicht is een samenvatting van ons intern register dat regelmatig wordt bijgewerkt.
+    </p>
+    <p>
+      Heb je een vraag over dit register of denk je dat er een verwerking ontbreekt, neem dan contact op met de functionaris van gegevensbescherming (DPO) via het <a href="https://formulieren.stad.gent/productie4.2/formulier/nl-NL/DefaultEnvironment/scprivacystad.aspx">contactformulier</a> of <a href="mailto:privacy@stad.gent">privacy@stad.gent</a>.
+    </p>
+    <p>
+      Wens je meer informatie over hoe we met jouw persoonsgegevens omgaan?
+    </p>
+    <p>
       Lees dan de <a href="https://stad.gent/over-gent-en-het-stadsbestuur/vragen-suggesties-en-meldingen/met-respect-voor-uw-privacy">privacyverklaring op onze website</a>.
-    </p>
-    <p>
-      Heb je vragen, opmerkingen of klachten over jouw privacy? Neem contact op met de functionaris voor gegevensbescherming via het
-      <a href="https://formulieren.stad.gent/productie4.2/formulier/nl-NL/DefaultEnvironment/scprivacystad.aspx">contactformulier</a>
-      of <a href="mailto:privacy@stad.gent">privacy@stad.gent</a>.
     </p>
   </div>
 </template>
